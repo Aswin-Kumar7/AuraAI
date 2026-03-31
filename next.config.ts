31e52e34d5c89f92d07ccabc8246101f195400b1
Aswin Kumar <developer.aswin07@gmail.com>
2026-03-31 14:44:57 +0530

    refactor: restructure project by moving to root

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: [
-    "employed-options-routines-avon.trycloudflare.com",
-    "himself-prior-allowing-interests.trycloudflare.com"
+    "physicians-london-realistic-theology.trycloudflare.com ",
+    "during-effectiveness-mutual-tours.trycloudflare.com"
   ]
 };
 
